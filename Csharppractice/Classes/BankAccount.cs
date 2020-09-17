d602fbf05273b93f8c19b31e098faacd8e4d7676
Stephen Stewart <stephenstewart1@hotmail.com>
2020-09-17 10:36:00 +0100

    Adding properties with Private

---
+++ Csharppractice/Classes/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharppractice.Classes
+{
+    public class BankAccount
+    {
+        private double _balance { get; set; }
+        public double Balance
+        {
+            get
+            {
+                if (_balance < 1000000)
+                {
+                    return _balance;
+                }
+                else
+                {
+                    return 100000;
+                }
+                
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _balance = value;
+                }
+                else
+                {
+                    _balance = 0;
+                }
+                
+            }
+
+        }
+
+        //propfull
+
+        //private int myVar;
+        //public int MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+        public BankAccount()
+        {
+            Balance = 100;
+        }
+
+        public double AddBalance(double BalanceToBeAdded)
+        {
+            Balance += BalanceToBeAdded;
+            return Balance;
+        }
+    }
+}
---
+++ Csharppractice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csharppractice.Classes;
+
+namespace Csharppractice
+{
+    class Program
+    {
+
+        static double numberTwo = 2;
+        static void Main(string[] args)
+        {
+            //var huh = new SimpleMath();
+            //huh.Add(2, 3);
+
+            var bank = new BankAccount();
+            bank.AddBalance(100);
+            Console.WriteLine(bank.Balance);
+
+
+
+        }
+    }
+
+    public class SimpleMath
+    {
+        public void Add(int number1, int number2)
+        {
+            if (number1 == number2)
+            {
+                Console.WriteLine("Nice!!!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong!!");
+            }
+        }
+    }
+}
